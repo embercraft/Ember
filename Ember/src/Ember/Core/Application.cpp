a20663bc4ce6be6df1f3e493c0d8890bc7d7309e
Rakshit <rakshitx1@gmail.com>
2024-07-07 10:19:55 +0530

    missing wrappers in layerstack

--- Ember/src/Ember/Core/Application.cpp
+++ Ember/src/Ember/Core/Application.cpp
@@ -65,11 +65,13 @@ namespace Ember {
 		dispatcher.Dispatch<WindowMinimizedEvent>(BIND_EVENT_FN(OnWindowMinimized));
 		dispatcher.Dispatch<WindowRestoredEvent>(BIND_EVENT_FN(OnWindowRestored));
 
-		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
+		for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); ++it)
 		{
-			(*--it)->OnEvent(e);
+			(*it)->OnEvent(e);
 			if (e.Handled)
+			{
 				break;
+			}
 		}
 	}
 
