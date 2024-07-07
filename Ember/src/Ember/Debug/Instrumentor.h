e8ffcffed3f703d7e01abc3084df0821336be2a2
Rakshit <rakshitx1@gmail.com>
2024-07-07 10:40:47 +0530

    auto deducing `__PRETTY_FUNCTION__` for other compilers

--- Ember/src/Ember/Debug/Instrumentor.h
+++ Ember/src/Ember/Debug/Instrumentor.h
@@ -135,10 +135,29 @@ namespace Ember {
 
 #if EMBER_PROFILE
 
+    #if defined(__clang__) || defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+		#define EMBER_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__DMC__) && (__DMC__ >= 0x810)
+		#define EMBER_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__FUNCSIG__)
+		#define EMBER_FUNC_SIG __FUNCSIG__
+	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+		#define EMBER_FUNC_SIG __FUNCTION__
+	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+		#define EMBER_FUNC_SIG __FUNC__
+	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+		#define EMBER_FUNC_SIG __func__
+	#elif defined(__cplusplus) && (__cplusplus >= 201103)
+		#define EMBER_FUNC_SIG __func__
+	#else
+		#define EMBER_FUNC_SIG "EMBER_FUNC_SIG unknown!"
+	#endif
+
+
     #define EMBER_PROFILE_BEGIN_SESSION(name, filepath)     ::Ember::Instrumentor::Get().BeginSession(name, filepath)
     #define EMBER_PROFILE_END_SESSION()                     ::Ember::Instrumentor::Get().EndSession()
     #define EMBER_PROFILE_SCOPE(name)                       ::Ember::InstrumentationTimer timer##__LINE__(name)
-    #define EMBER_PROFILE_FUNCTION()                        EMBER_PROFILE_SCOPE(__PRETTY_FUNCTION__)
+    #define EMBER_PROFILE_FUNCTION()                        EMBER_PROFILE_SCOPE(EMBER_FUNC_SIG)
 
 #else
 
